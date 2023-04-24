8b2e85c7e50f25d9b7df43ef7d58dfa28b1a4847
Alexey Karlyganov <adkarlyganov@gmail.com>
2023-04-24 18:50:38 +0300

    fix nullable fields in `Incident`

--- src/summary/incident.rs
+++ src/summary/incident.rs
@@ -6,13 +6,13 @@ use super::component::AffectedComponent;
 pub struct Incident {
     pub id: String,
     pub status: String,
-    pub body: String,
-    pub incident_id: String,
+    pub body: Option<String>,
+    pub incident_id: Option<String>,
     pub created_at: String,
     pub updated_at: String,
-    pub display_at: String,
+    pub display_at: Option<String>,
     pub affected_components: Option<Vec<AffectedComponent>>,
-    pub deliver_notifications: bool,
+    pub deliver_notifications: Option<bool>,
     pub custom_tweet: Option<String>,
     pub tweet_id: Option<String>
 }
